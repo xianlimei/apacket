d0d7d5c477ac1cc9bcad67bf84882f0c1eda3544
acey9 <huiwang.e@gmail.com>
2017-05-03 22:55:15 +0800

    change dir tree.

---
+++ outputs/output.go
@@ -0,0 +1,136 @@
+package outputs
+
+import (
+	"encoding/json"
+	"github.com/Acey9/apacket/config"
+	"github.com/Acey9/apacket/decoder"
+	"github.com/Acey9/apacket/logp"
+	"github.com/tsg/gopacket/layers"
+)
+
+type Outputer struct {
+	pktQueue    chan *decoder.Packet
+	filterQueue chan *decoder.Packet
+	session     *Session
+}
+
+func NewOutputer() *Outputer {
+	s := NewSesson()
+	o := &Outputer{pktQueue: make(chan *decoder.Packet),
+		filterQueue: make(chan *decoder.Packet),
+		session:     s}
+	return o
+}
+
+func (out *Outputer) PublishEvent(pkt *decoder.Packet) {
+	if config.Cfg.Backscatter {
+		out.filterQueue <- pkt
+	} else {
+		out.pktQueue <- pkt
+	}
+}
+
+func (out *Outputer) output(pkt *decoder.Packet) {
+	b, err := json.Marshal(pkt)
+	if err != nil {
+		logp.Err("%s", err)
+	}
+	logp.Info("pkt %s", b)
+}
+
+func (out *Outputer) filterTCP(pkt *decoder.Packet) *decoder.Packet {
+
+	//ignore not syn and not syn_ack
+	if !pkt.Tcp.SYN && !(pkt.Tcp.SYN && pkt.Tcp.ACK) {
+		return nil
+	}
+
+	if pkt.Tcp.SYN && pkt.Tcp.ACK { //syn_ack
+		//ignore device sended syn_ack
+		_, ok := config.Cfg.IfaceAddrs[pkt.Flow.Sip.String()]
+		if ok {
+			return nil
+		}
+
+		//ignore device syn response(syn_ack)
+		flowid := pkt.Flow.ToOutFlowID()
+		if out.session.QuerySession(flowid) {
+			out.session.DeleteSession(flowid)
+			logp.Debug("filter", "device syn_ack, flow id:%s", pkt.Flow.ToOutFlowID())
+			return nil
+		}
+	} else if pkt.Tcp.SYN { //syn
+
+		//ignore device syn
+		_, ok := config.Cfg.IfaceAddrs[pkt.Flow.Sip.String()]
+		if ok {
+			logp.Debug("filter", "device syn, flow id:%s", pkt.Flow.FlowID())
+			out.session.AddSession(pkt.Flow.FlowID())
+			return nil
+		}
+	}
+	return pkt
+}
+
+func (out *Outputer) filterUDP(pkt *decoder.Packet) *decoder.Packet {
+	//ignore device sended udp
+	_, ok := config.Cfg.IfaceAddrs[pkt.Flow.Sip.String()]
+	if ok {
+		logp.Debug("filter", "device udp, flow id:%s", pkt.Flow.FlowID())
+		out.session.AddSession(pkt.Flow.FlowID())
+		return nil
+	} else {
+		//ignore device udp response
+		flowid := pkt.Flow.ToOutFlowID()
+		if out.session.QuerySession(flowid) {
+			out.session.DeleteSession(flowid)
+			logp.Debug("filter", "device udp response, flow id:%s", pkt.Flow.ToOutFlowID())
+			return nil
+		}
+	}
+	return pkt
+}
+
+func (out *Outputer) filter(pkt *decoder.Packet) *decoder.Packet {
+	switch pkt.PktType {
+	case decoder.PktTypeTCP:
+		p := out.filterTCP(pkt)
+		if p == nil {
+			return nil
+		}
+		return pkt
+	case decoder.PktTypeUDP:
+		p := out.filterUDP(pkt)
+		if p == nil {
+			return nil
+		}
+		return pkt
+	case decoder.PktTypeICMPv4:
+		//TODO
+	case decoder.PktTypeDNS:
+		//ignore ptr query
+		if len(pkt.Dns.Questions) > 0 && pkt.Dns.Questions[0].Type == layers.DNSTypePTR {
+			return nil
+		}
+		p := out.filterUDP(pkt)
+		if p == nil {
+			return nil
+		}
+		return pkt
+	}
+	return pkt
+}
+
+func (out *Outputer) Start() {
+	for {
+		select {
+		case pkt := <-out.pktQueue:
+			out.output(pkt)
+		case pkt := <-out.filterQueue:
+			p := out.filter(pkt)
+			if p != nil {
+				out.output(pkt)
+			}
+		}
+	}
+}
---
+++ outputs/session.go
@@ -0,0 +1,48 @@
+package outputs
+
+import (
+	"github.com/Acey9/apacket/logp"
+	"time"
+)
+
+const SessionExpired = 5
+
+type Session struct {
+	tab map[string]time.Time
+}
+
+func NewSesson() *Session {
+	s := &Session{tab: make(map[string]time.Time)}
+	go s.clean()
+	return s
+}
+
+func (s *Session) AddSession(flowid string) {
+	s.tab[flowid] = time.Now()
+}
+
+func (s *Session) QuerySession(flowid string) bool {
+	_, ok := s.tab[flowid]
+	if ok {
+		return true
+	}
+	return false
+}
+
+func (s *Session) DeleteSession(flowid string) {
+	delete(s.tab, flowid)
+}
+
+func (s *Session) clean() {
+	sleep := time.Millisecond * time.Duration(1000)
+	for {
+		logp.Debug("session", "session map len:%d", len(s.tab))
+		for k, v := range s.tab {
+			if time.Since(v) > time.Second*SessionExpired {
+				logp.Debug("session", "delete session id:%s", k)
+				s.DeleteSession(k)
+			}
+		}
+		time.Sleep(sleep)
+	}
+}
