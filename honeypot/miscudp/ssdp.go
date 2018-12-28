423fccc30a44e0130d3900ebc931212936ab4c88
acey9 <huiwang.e@gmail.com>
2018-12-28 14:13:55 +0800

    update

--- honeypot/miscudp/ssdp.go
+++ honeypot/miscudp/ssdp.go
@@ -17,10 +17,10 @@ func (ssdp *SSDP) Response() (resp []byte) {
 	buf.Write([]byte("HTTP/1.1 200 OK\r\n"))
 	buf.Write([]byte("Server: Custom/1.0 UPnP/1.0 Proc/Ver\r\n"))
 	buf.Write([]byte("EXT:\r\n"))
-	buf.Write([]byte("Location: http://192.168.1.1:5431/dyndev/uuid:38e59522-dd48-48dd-2295-e538e522480000\r\n"))
+	buf.Write([]byte("Location: http://192.168.1.1:5431/dyndev/uuid:681ab2eb-8b10-108b-ebb2-1a681aeb100000\r\n"))
 	buf.Write([]byte("Cache-Control:max-age=1800\r\n"))
 	buf.Write([]byte("ST:ssdp:all\r\n"))
-	buf.Write([]byte("USN:uuid:38e59522-dd48-48dd-2295-e538e522480000::upnp:rootdevice\r\n"))
+	buf.Write([]byte("USN:uuid:681ab2eb-8b10-108b-ebb2-1a681aeb100000::upnp:rootdevice\r\n"))
 	buf.Write([]byte("\r\n"))
 	resp = buf.Bytes()
 	return
